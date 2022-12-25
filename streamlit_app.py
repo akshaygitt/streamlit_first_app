3d29b0052752e787791add3cfd9fdb6a69acafc7
Akshay Baishander <60030900+akshaygitt@users.noreply.github.com>
2022-12-25 23:27:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 
 # st.title('This is my first program in github')
-# st.header('Breakfast menu of Royal Hotel')
-# st.text('Poha and Masala Tea')
-# st.text('Samosa and Kachori')
-# st.text('Edli and Sambhar') 
+st.header('Breakfast menu of Royal Hotel')
+st.text('Poha and Masala Tea')
+st.text('Samosa and Kachori')
+st.text('Edli and Sambhar') 
+st.title('Here we are taking data from amazon s3 bucket') 
 my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruits_list = my_fruit_list.set_index('Fruit')
 st.multiselect("Pick any fruits from this list: ", list(my_fruits_list.index))
 st.dataframe(my_fruits_list)
 
