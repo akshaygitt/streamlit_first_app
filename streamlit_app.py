176a85a003e3b030ef2ea5add6fb81396a67ef18
Akshay Baishander <60030900+akshaygitt@users.noreply.github.com>
2022-12-25 23:20:45 +0530

    Update streamlit_app.py
    reading csv file from amazon s3 bucket and reading content of it using pandas library of python

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 # st.title('This is my first program in github')
 st.header('Breakfast menu of Royal Hotel')
 st.text('Poha and Masala Tea')
 st.text('Samosa and Kachori')
 st.text('Edli and Sambhar') 
+my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruits_list)
+
 
