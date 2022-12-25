478dfd2c5fcd91d79a09eb14017eb56b489bab65
Akshay Baishander <60030900+akshaygitt@users.noreply.github.com>
2022-12-25 23:15:47 +0530

    Update streamlit_app.py
    Making breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.title('This is my first program in github')
+# st.title('This is my first program in github')
+st.header('Breakfast menu of Royal Hotel')
+st.text('Poha and Masala Tea')
+st.text('Samosa and Kachori')
+st.text('Edli and Sambhar') 
+
