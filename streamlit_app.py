e2a68c7f9e75e05ddc4a7d09fcaeeae1438169da
Akshay Baishander <60030900+akshaygitt@users.noreply.github.com>
2022-12-25 23:32:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ st.text('Poha and Masala Tea')
 st.text('Samosa and Kachori')
 st.text('Edli and Sambhar') 
 st.title('Here we are taking data from amazon s3 bucket') 
-my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruits_list = my_fruit_list.set_index('Fruit')
-st.multiselect("Pick any fruits from this list: ", list(my_fruits_list.index))
-st.dataframe(my_fruits_list)
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+st.multiselect("Pick any fruits from this list: ", list(my_fruit_list.index))
+st.dataframe(my_fruit_list)
 
 
